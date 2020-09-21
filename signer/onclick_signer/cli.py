fa3e82c5ad127420195a33f096a5d2f9b3741392
Mike Shultz <shultzm@gmail.com>
2020-09-20 22:26:58 -0600

    add ability to get password from console

--- signer/onclick_signer/cli.py
+++ signer/onclick_signer/cli.py
@@ -1,4 +1,6 @@
+import os
 import sys
+from getpass import getpass
 import tornado.ioloop
 from argparse import ArgumentParser
 from onclick_signer.web import make_app
@@ -11,6 +13,11 @@ def parse_args(argv):
 
 def main(argv=sys.argv[1:]):
     args = parse_args(argv)
+
+    # Take passphrase at console instead of using env vars
+    if not os.environ.get('ENCRYPTION_PASSPHRASE'):
+        decrypt = getpass(prompt="Decrypt passphrase: ")
+        os.environ['ENCRYPTION_PASSPHRASE'] = decrypt
 
     app = make_app()
     app.listen(args.port)
